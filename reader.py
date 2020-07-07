396cf66188b838b7d53259e9583db9f84523709b
Haruka Yamashita <hyamashita@nig.ac.jp>
2020-07-07 18:48:26 +0900

    Comment out unnecessary functions

--- reader.py
+++ reader.py
@@ -1,16 +1,16 @@
-from pathmanage.classes import SFSDirMap
+# from pathmanage.classes import SFSDirMap
 
-def load_SFSDirMap_from_data_paths(csv_path, pickle_path):
-    """ Returns SFSDirMap instance by loading existing csv and pickle files. """
+# def load_SFSDirMap_from_data_paths(csv_path, pickle_path):
+#     """ Returns SFSDirMap instance by loading existing csv and pickle files. """
 
-    return SFSDirMap(csv_path=csv_path, pickle_path=pickle_path)
+#     return SFSDirMap(csv_path=csv_path, pickle_path=pickle_path)
 
-def get_SFSDirMap_from_top_dir(top, sfs_format, site_type_pos, 
-                               scale=100, description='', gene_list=[], 
-                               gene_match_func=None):
+# def get_SFSDirMap_from_top_dir(top, sfs_format, site_type_pos, 
+#                                scale=100, description='', gene_list=[], 
+#                                gene_match_func=None):
 
-    return SFSDirMap(
-        top=top, sfs_format=sfs_format, site_type_pos=site_type_pos,
-        scale=scale, description=description, gene_list=gene_list, 
-        gene_match_func=gene_match_func
-    )
+#     return SFSDirMap(
+#         top=top, sfs_format=sfs_format, site_type_pos=site_type_pos,
+#         scale=scale, description=description, gene_list=gene_list, 
+#         gene_match_func=gene_match_func
+#     )
